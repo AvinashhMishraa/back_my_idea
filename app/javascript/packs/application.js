7bda73b85cbd7e31d91ee920c181dec394da5727
AvinashhMishraa <avinashhkumarrmishraa@gmail.com>
2021-11-08 20:18:35 +0530

    expandable searchbar + shopping cart icon with item count+ highlighting current page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,7 @@
 // ActiveStorage.start()
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
